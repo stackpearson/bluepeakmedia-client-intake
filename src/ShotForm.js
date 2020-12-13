dc3b13a64d8262fe6e3166538ebb40130e5c5ea5
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-12-12 21:37:41 -0700

    updated photo upload state

--- src/ShotForm.js
+++ src/ShotForm.js
@@ -1,9 +1,9 @@
-import React, {useState,} from 'react';
-import { Col, Row, Button, Form, FormGroup, Input,Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import {connect} from 'react-redux';
+import React, { useState, } from 'react';
+import { Col, Row, Button, Form, FormGroup, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { connect } from 'react-redux';
 import Front from './front.png';
-import {removeShot} from './actions/shotActions';
-import {updateId} from './actions/shotActions';
+import { removeShot } from './actions/shotActions';
+import { updateId } from './actions/shotActions';
 
 const ShotForm = (props) => {
 
@@ -19,10 +19,11 @@ const ShotForm = (props) => {
     const [shotDetails, setShotDetails] = useState({
         pictureId: null,
         shot_description: null,
-        shot_notes: null
+        shot_notes: null,
+        shot_type: null
     });
 
-    const updateImgId = ()  => {
+    const updateImgId = () => {
         let Ids = {
             initialIdx: props.initialId - 1,
             currentId: props.pictureId
@@ -39,80 +40,86 @@ const ShotForm = (props) => {
         setShotDetails(newShotDetails)
     }
 
+    const handlePhotoType = (selection) => {
+        shotDetails.shot_type = selection
+    }
+
     const handleFileSelect = event => {
+        // console.log(event.target.files[0])
         setShotImage({
-            img_preview: URL.createObjectURL(event.target.files[0]), 
+            img_preview: URL.createObjectURL(event.target.files[0]),
             img_src: event.target.files[0]
+
         })
     }
 
-  return (
-      
-      <div className='shot_detail-form'>
-          
-        <Form>
-            <div className='shot_detail-header'>
-                <div>
-                    <p>Picture {props.pictureId}</p>
+    return (
+
+        <div className='shot_detail-form'>
+
+            <Form>
+                <div className='shot_detail-header'>
+                    <div>
+                        <p>Picture {props.pictureId}</p>
+                    </div>
+                    <div>
+                        {props.pictureId > 1 ? (
+                            <span className='shot_form-delete' onClick={() => { props.removeShot(props.pictureId); updateImgId(); }}>X</span>
+                        ) : (<></>)}
+                    </div>
                 </div>
-                <div>
-                    { props.pictureId > 1 ? (
-                        <span onClick={() => {props.removeShot(props.pictureId); updateImgId();}}>X</span>
-                    ) : (<></>)}
-                </div>
-            </div>
-            
-            <div className='shot_detail-formFlex'>
-                <div className='shot_detail-formFile'>
-                    <FormGroup row>
-                        <img alt='front facing lifestyle view of a product' className='shot_detail-exampleImg' src={shotImage.img_preview} />
-                        <Col sm={10}>
-                            <input type="file" onChange={handleFileSelect} />
-                            <Button>Upload</Button>
-                        </Col>
-                    </FormGroup>
-                </div>
-                <div className='shot_detail-formText'>                
-                    <FormGroup>
-                        <Row xs='0'>
-                            <Input onChange={handleChanges} type="textarea" name="shot_description" placeholder='Shot Description - ex: Angled front view lifestyle shot of product holding makeup brushes.' />
-                        </Row>
-                    </FormGroup>
-                    
-                    <FormGroup>
-                        <Row>
-                            <Input onChange={handleChanges} type="textarea" name="shot_notes" placeholder='Notes: Anything else you would like to convey to us' />
-                        </Row>
-                    </FormGroup>
-                    <FormGroup>
-                        <Row>
-                        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                            <DropdownToggle caret>
-                                Image Appearances
+
+                <div className='shot_detail-formFlex'>
+                    <div className='shot_detail-formFile'>
+                        <FormGroup row>
+                            <img alt='front facing lifestyle view of a product' className='shot_detail-exampleImg' src={shotImage.img_preview} />
+                            <Col sm={10}>
+                                <input type="file" onChange={handleFileSelect} />
+                                {/* <Button>Upload</Button> */}
+                            </Col>
+                        </FormGroup>
+                    </div>
+                    <div className='shot_detail-formText'>
+                        <FormGroup>
+                            <Row xs='0'>
+                                <Input onChange={handleChanges} type="textarea" name="shot_description" placeholder='Shot Description - ex: Angled front view lifestyle shot of product holding makeup brushes.' />
+                            </Row>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Row>
+                                <Input onChange={handleChanges} type="textarea" name="shot_notes" placeholder='Notes: Anything else you would like to convey to us' />
+                            </Row>
+                        </FormGroup>
+                        <FormGroup>
+                            <Row>
+                                <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                                    <DropdownToggle caret>
+                                        Image Appearances
                             </DropdownToggle>
-                            <DropdownMenu>
-                                <DropdownItem>Match Example</DropdownItem>
-                                <DropdownItem>Similar to Example</DropdownItem>
-                                <DropdownItem>Photogs Choice</DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                        </Row>
-                    </FormGroup>
+                                    <DropdownMenu>
+                                        <DropdownItem onClick={() => { handlePhotoType('Match Example'); }}>Match Example</DropdownItem>
+                                        <DropdownItem>Similar to Example</DropdownItem>
+                                        <DropdownItem>Photogs Choice</DropdownItem>
+                                    </DropdownMenu>
+                                </Dropdown>
+                            </Row>
+                        </FormGroup>
+                    </div>
                 </div>
-            </div>
-        </Form>
-    </div>
-  );
+            </Form>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
     return {
         shotOnProps: state.shotReducer
-        
+
     }
 }
 
 export default connect(
     mapStateToProps,
-    {removeShot, updateId}
+    { removeShot, updateId }
 )(ShotForm)
